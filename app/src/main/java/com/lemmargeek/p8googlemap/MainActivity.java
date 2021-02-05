56d2e495db25c5f383ef67adc17d81f36a00833c
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2021-02-05 18:35:23 -0500

    Google Maps

--- app/src/main/java/com/lemmargeek/p8googlemap/MainActivity.java
+++ app/src/main/java/com/lemmargeek/p8googlemap/MainActivity.java
@@ -1,14 +1,26 @@
 package com.lemmargeek.p8googlemap;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap = googleMap;
     }
 }
