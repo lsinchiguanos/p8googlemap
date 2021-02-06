a2a4777aa2389b9b24363a960a1a90f6a9269427
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2021-02-05 19:24:48 -0500

    Dos opciones

--- app/src/main/java/com/lemmargeek/p8googlemap/MainActivity.java
+++ app/src/main/java/com/lemmargeek/p8googlemap/MainActivity.java
@@ -2,9 +2,14 @@ package com.lemmargeek.p8googlemap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -23,4 +28,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         googleMap = googleMap;
     }
+
+    public void changedViewMap(View view){
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    public void moveCamMap(View view){
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(40.41, -3.69), 5);
+        googleMap.moveCamera(cameraUpdate);
+    }
+
 }
